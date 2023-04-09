62787b4db93ddf1778f317c78ff0b9eb9e0b5334
nttt2004 <124849254+nttt2004@users.noreply.github.com>
2023-04-09 23:13:03 +0700

    Update Pipes.h

--- Pipes.h
+++ Pipes.h
@@ -1,1 +1,18 @@
+#ifndef PIPES_H_
+#define PIPES_H_
 
+#include "CommonFunction.h"
+#include "ImageObjectBase.h"
+
+#define PIPE_WIDTH 52
+#define PIPE_HEIGHT  320
+#define PIPE_GAP  100
+#define PIPE_SPEED  125
+
+class Pipes : public ImageObjectBase
+{
+public:
+    
+private:
+    
+};
