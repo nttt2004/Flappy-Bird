f5017445451aaa58a75145ca672b9a78b4f58840
nttt2004 <124849254+nttt2004@users.noreply.github.com>
2023-05-10 15:28:48 +0700

    Update Base.cpp

--- Base.cpp
+++ Base.cpp
@@ -20,6 +20,12 @@ bool Base::LoadImg(std::string path, SDL_Renderer* screen)
 
 void Base::Show(SDL_Renderer* des)
 {
-    SDL_Rect base_rect = {gBaseX, gBaseY, SCREEN_WIDTH, BASE_HEIGHT};
+    gBaseX -= BASE_SPEED;
+    if(gBaseX < - SCREEN_WIDTH)
+    {
+        gBaseX = 0;
+    }
+    SDL_Rect base_rect = {gBaseX, gBaseY, BASE_WIDTH, BASE_HEIGHT};
     SDL_RenderCopy(des, p_object_, NULL, &base_rect);
 }
+
